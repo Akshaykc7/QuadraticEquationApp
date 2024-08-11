df6cc3ebdff60a94eb71d2e27bcc3fa121bd956d
Akshay K C <akshaykc016@gmail.com>
2024-08-11 23:05:40 +0530

    QuadraticEquationApp

---
+++ app/src/main/java/com/example/quadraticequationapp/MyEquation.java
@@ -0,0 +1,77 @@
+package com.example.quadraticequationapp;
+
+import android.view.View;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+import com.example.quadraticequationapp.databinding.ActivityMainBinding;
+
+public class MyEquation extends BaseObservable {
+
+    String a;
+    String b;
+    String c;
+    ActivityMainBinding activityMainBinding;
+
+    public MyEquation(ActivityMainBinding activityMainBinding) {
+        this.activityMainBinding = activityMainBinding;
+    }
+
+    public MyEquation() {
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    @Bindable
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    @Bindable
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    @Bindable
+    public String getC() {
+        return c;
+    }
+
+    public void setC(String c) {
+        this.c = c;
+    }
+
+    public void solveEquation(View view){
+        int a = Integer.parseInt(getA());
+        int b = Integer.parseInt(getB());
+        int c = Integer.parseInt(getC());
+
+        //Calculating the discriminant
+        double d = ((b*b) - 4*(a*c));
+
+        //Finding the roots
+        double x1, x2;
+        if(d > 0){
+            x1 = (-b + Math.sqrt(d))/(2*a);
+            x2 = (-b - Math.sqrt(d))/(2*a);
+
+            //Display the roots in textview
+            activityMainBinding.textView.setText("X1 = " + x1 + " X2 = " +x2);
+        } else if (d < 0) {
+            activityMainBinding.textView.setText("No real root");
+        }
+        else {
+            x1 = -b/(2*a);
+            activityMainBinding.textView.setText("X1 = " + x1);
+        }
+
+
+    }
+}
